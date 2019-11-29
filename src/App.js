9d87ee0e5148d0896319c8ae39f9ed714629d586
Mikael <hej_m@hotmail.com>
2019-11-29 11:48:48 +0100

    Created the project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import prototyp from './images/prototyp.png';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <img src={prototyp}/>
+    </div>
+  );
+}
+
+export default App;
