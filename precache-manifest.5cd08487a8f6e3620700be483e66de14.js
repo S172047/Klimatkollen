26a58d153556d668ef29a868b08aa1d15675a7a4
Simon <s171927@student.hb.se>
2019-12-03 18:57:33 +0100

    Updates

---
+++ precache-manifest.5cd08487a8f6e3620700be483e66de14.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "113aca921bca7ce0282abb03661f9187",
+    "url": "/Klimatkollen/index.html"
+  },
+  {
+    "revision": "eea3241997ea6830a173",
+    "url": "/Klimatkollen/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "b5bb8a536e483f200e6a",
+    "url": "/Klimatkollen/static/css/main.a522ae9c.chunk.css"
+  },
+  {
+    "revision": "eea3241997ea6830a173",
+    "url": "/Klimatkollen/static/js/2.9d1c485b.chunk.js"
+  },
+  {
+    "revision": "b5bb8a536e483f200e6a",
+    "url": "/Klimatkollen/static/js/main.53ecc961.chunk.js"
+  },
+  {
+    "revision": "91b4f6b91fba73907497",
+    "url": "/Klimatkollen/static/js/runtime-main.3d194f67.js"
+  },
+  {
+    "revision": "d6710775d399e0d1cd2087fe95edc935",
+    "url": "/Klimatkollen/static/media/bg_rymd.d6710775.jpg"
+  },
+  {
+    "revision": "ed7fa48be136c677abc3052775c1a32e",
+    "url": "/Klimatkollen/static/media/button_glaciar.ed7fa48b.png"
+  },
+  {
+    "revision": "c7ec8b9b79229ac3e9a7e5b5fba7cf46",
+    "url": "/Klimatkollen/static/media/button_koldioxid.c7ec8b9b.png"
+  },
+  {
+    "revision": "164c80af1d701f0f18ad18aba4f6744c",
+    "url": "/Klimatkollen/static/media/button_temp.164c80af.png"
+  },
+  {
+    "revision": "ce41be948b61782e0cc2c92d4975f5e0",
+    "url": "/Klimatkollen/static/media/jordglob.ce41be94.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Klimatkollen/precache-manifest.6033e84ef30f3ce71f20f4e81a7146f5.js"
+  "/Klimatkollen/precache-manifest.5cd08487a8f6e3620700be483e66de14.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Klimatkollen/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
