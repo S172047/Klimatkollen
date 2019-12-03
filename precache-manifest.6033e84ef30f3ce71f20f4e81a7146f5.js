0dd40a67fe946da3830aad8fd62da8c5b50fcc41
Sanna Svensson <s172047@student.hb.se>
2019-12-03 12:37:58 +0100

    Updates

---
+++ precache-manifest.6033e84ef30f3ce71f20f4e81a7146f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fa85da113b8c701087c45c2f4b6b034",
+    "url": "/Klimatkollen/index.html"
+  },
+  {
+    "revision": "5e12333ea6c5ebf83541",
+    "url": "/Klimatkollen/static/css/main.5e4e9510.chunk.css"
+  },
+  {
+    "revision": "c291c00b6ef1a8375499",
+    "url": "/Klimatkollen/static/js/2.560cdd7a.chunk.js"
+  },
+  {
+    "revision": "5e12333ea6c5ebf83541",
+    "url": "/Klimatkollen/static/js/main.de3ab1c0.chunk.js"
+  },
+  {
+    "revision": "91b4f6b91fba73907497",
+    "url": "/Klimatkollen/static/js/runtime-main.3d194f67.js"
+  },
+  {
+    "revision": "92f9e7198aa3bed9feb37e33ea3ac616",
+    "url": "/Klimatkollen/static/media/prototyp.92f9e719.png"
+  }
+]);
