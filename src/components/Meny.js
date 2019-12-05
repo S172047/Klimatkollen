d6346bb2e87d77fa52d41267ff8329db9a489aef
Simon <s171927@student.hb.se>
2019-12-05 15:51:05 +0100

    Simon och Mikael

--- src/components/Meny.js
+++ src/components/Meny.js
@@ -2,30 +2,31 @@ import React, { Component } from 'react'
 import { Navbar, Nav, NavItem, Button, DropdownButton, Dropdown } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import './Stil.css'
+import Logga from '../images/logga.png'
 
 export default class Meny extends Component {
     render() {
         return (
                 <Navbar default collapseOnSelect>
                         <Navbar.Brand>
-                            <h1>Klimatkollen</h1>
+                        <img className="klimatlogga" src={Logga} alt="Logotype" />
                         </Navbar.Brand>
                     <Navbar.Toggle />
                     <Navbar.Collapse>
                         <Nav className="ml-auto">
                             <DropdownButton id="dropdown-basic-button" title="Meny">
-                            <Dropdown.Item href="#/action-1">Koldioxid</Dropdown.Item>
-                            <Dropdown.Item href="#/action-2">Temperatur</Dropdown.Item>
-                            <Dropdown.Item href="#/action-3">Glaciärer</Dropdown.Item>
+                            <Dropdown.Item href="#/action-1">Utsläpp</Dropdown.Item>
+                            <Dropdown.Item href="#/action-2">Temperaturer</Dropdown.Item>
+                            <Dropdown.Item href="#/action-3">Havsnivåer</Dropdown.Item>
                             </DropdownButton>
                                 <Link className="linkstyle" to="/koldioxid">
-                                <Button variant="secondary" size="sm">KOLDIOXID</Button>
+                                <Button className="btn-meny" variant="secondary" size="sm">UTSLÄPP</Button>
                                 </Link>
                                 <Link className="linkstyle" to="/temperatur">
-                                <Button variant="secondary" size="sm">TEMPERATUR</Button>
+                                <Button className="btn-meny" variant="secondary" size="sm">TEMPERATURER</Button>
                                 </Link>
                                 <Link className="linkstyle" to="/glaciar">
-                                <Button variant="secondary" size="sm">GLACIÄRER</Button>
+                                <Button className="btn-meny" variant="secondary" size="sm">HAVSNIVÅER</Button>
                                 </Link>
                         </Nav>
                     </Navbar.Collapse>
