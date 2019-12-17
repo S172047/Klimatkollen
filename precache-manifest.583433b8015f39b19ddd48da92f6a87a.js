736c107f2c9a61b9d544beae91eaf553272060e9
Simon <s171927@student.hb.se>
2019-12-17 15:11:07 +0100

    Updates

--- precache-manifest.583433b8015f39b19ddd48da92f6a87a.js
+++ precache-manifest.583433b8015f39b19ddd48da92f6a87a.js
@@ -1,27 +1,31 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6b98b7cc7599c71e9f95530e4b6af8e4",
+    "revision": "ba54661ab1fb4f3a454e115eee61f29f",
     "url": "/Klimatkollen/index.html"
   },
   {
-    "revision": "7d215586d92b60bfaea0",
+    "revision": "c4f4d677702688829011",
     "url": "/Klimatkollen/static/css/2.17e5ed98.chunk.css"
   },
   {
-    "revision": "70ecedc9f66610ccc31f",
-    "url": "/Klimatkollen/static/css/main.6d7a7ca8.chunk.css"
+    "revision": "15e695f4a2f9cfd38a8c",
+    "url": "/Klimatkollen/static/css/main.319e91fc.chunk.css"
   },
   {
-    "revision": "7d215586d92b60bfaea0",
-    "url": "/Klimatkollen/static/js/2.63a8c3ee.chunk.js"
+    "revision": "c4f4d677702688829011",
+    "url": "/Klimatkollen/static/js/2.28ca6d22.chunk.js"
   },
   {
-    "revision": "70ecedc9f66610ccc31f",
-    "url": "/Klimatkollen/static/js/main.18687b46.chunk.js"
+    "revision": "15e695f4a2f9cfd38a8c",
+    "url": "/Klimatkollen/static/js/main.7b4b3439.chunk.js"
   },
   {
     "revision": "91b4f6b91fba73907497",
     "url": "/Klimatkollen/static/js/runtime-main.3d194f67.js"
+  },
+  {
+    "revision": "0c481e87f2774b1bd41a0a70d9b70d11",
+    "url": "/Klimatkollen/static/media/Test.0c481e87.mp3"
   },
   {
     "revision": "afcd91e4c22234c0b01ae7b45557a12a",
@@ -38,6 +42,10 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "5530ad7ff237d50352af93223f7e80fd",
     "url": "/Klimatkollen/static/media/closebutton.5530ad7f.png"
+  },
+  {
+    "revision": "33818ad75c5d988c49c3489d30e9aba7",
+    "url": "/Klimatkollen/static/media/isberg.33818ad7.jpeg"
   },
   {
     "revision": "c2cd7780a5c610f0b57fe9a8dbaca720",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Klimatkollen/precache-manifest.ff43286ab5b09abc7feafb07d860b4cd.js"
+  "/Klimatkollen/precache-manifest.583433b8015f39b19ddd48da92f6a87a.js"
 );
 
 self.addEventListener('message', (event) => {
